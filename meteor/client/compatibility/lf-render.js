b84457ef0f7351f4eef2628b6b26888c19540a08
Petr Baudis <pasky@ucw.cz>
2013-07-23 03:54:44 +0200

    Lightfield image view: Add simple zooming support

--- meteor/client/compatibility/lf-render.js
+++ meteor/client/compatibility/lf-render.js
@@ -240,8 +240,9 @@ LightFieldRenderer.prototype.render_image = function(canvas, gl) {
 	gl.useProgram(program);
 
 	// set(up) parameters
+	var zoom = Math.pow(10, parseFloat(Session.get("currentImageZoom")));
 	var canvSizeLocation = gl.getUniformLocation(program, "u_canvSize");
-	gl.uniform2f(canvSizeLocation, canvas.width, canvas.height);
+	gl.uniform2f(canvSizeLocation, canvas.width / zoom, canvas.height / zoom);
 	var gammaGainLocation = gl.getUniformLocation(program, "u_gammaGain");
 	gl.uniform2f(gammaGainLocation, parseFloat(Session.get("currentImageGamma")), Math.pow(10, parseFloat(Session.get("currentImageGain"))));
 
@@ -318,6 +319,7 @@ LightFieldRenderer.prototype.render_grid = function(canvas, gl) {
 		"width": Math.ceil(this.image.width / this.lenslets.right[0]),
 		"height": Math.ceil(this.image.height / this.lenslets.down[1])
 	};
+	console.log("grid corner " + gridCorner + " size " + gridSize);
 	var lineList = new Array;
 	for (var x = 0; x <= gridSize.width; x++) {
 		lineList.push(gridCorner[0] + x * this.lenslets.right[0]);
@@ -339,8 +341,9 @@ LightFieldRenderer.prototype.render_grid = function(canvas, gl) {
 	gl.enableVertexAttribArray(canvCoordLocation);
 	gl.vertexAttribPointer(canvCoordLocation, 2, gl.FLOAT, false, 0, 0);
 
+	var zoom = Math.pow(10, parseFloat(Session.get("currentImageZoom")));
 	var canvSizeLocation = gl.getUniformLocation(program, "u_canvSize");
-	gl.uniform2f(canvSizeLocation, canvas.width, canvas.height);
+	gl.uniform2f(canvSizeLocation, canvas.width / zoom, canvas.height / zoom);
 
 	gl.drawArrays(gl.LINES, 0, lineList.length / 2);
 }
