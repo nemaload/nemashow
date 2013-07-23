91d7479b0cd98cf64b32f06a6e9052cb4d965744
Michael Schmatz <schmatz@umich.edu>
2013-07-22 17:23:33 -0700

    Added client-side subscriptions

--- meteor/client/subscriptions.js
+++ meteor/client/subscriptions.js
@@ -1,1 +1,7 @@
 Meteor.subscribe("directory");
+Meteor.subscribe("images");
+Meteor.subscribe("folders", function onReady() {
+  Session.set('loadedLiveData',true);
+});
+
+Meteor.subscribe("annotations");
