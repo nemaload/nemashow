e27d39e54e480a0b5b84ad67d9e5bca650009aac
Michael Schmatz <schmatz@umich.edu>
2013-07-22 17:26:56 -0700

    Added support for preloading

--- meteor/client/templates/folders_main/folders_main.js
+++ meteor/client/templates/folders_main/folders_main.js
@@ -1,21 +1,24 @@
 Template.folders_main.children = function(parentId) {
-	//call stack error lies here
-	return Folders.find({
-		parent: parentId
-	});
+        //call stack error lies here
+        if (Session.get("loadedLiveData")) {
+                return Folders.find({
+                        parent: parentId
+                });
+        }
+        else
+                return localCollection.find({parent: parentId});
+
 }
 
 Template.folders_main.hasChildren = function(parentId) {
-	var numFolders = Folders.find({
-		parent: parentId
-	}).count();
-	if (numFolders > 0) {
-		return true;
-	} else {
-		return false;
-	}
+        var numFolders = (Session.get("loadedLiveData")) ? Folders.find({parent: parentId}).count() : localCollection.find({parent: parentId}).count();
+        if (numFolders > 0) {
+                return true;
+        } else {
+                return false;
+        }
 }
 
 Template.folders_main.isCurrentFolder = function(folder) {
-	return Template.folders.isCurrentFolder(folder);
+        return Template.folders.isCurrentFolder(folder);
 }
