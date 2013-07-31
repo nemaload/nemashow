d84d34abd2aa7e277d383a801aafc0653b30a0ed
Michael Schmatz <schmatz@umich.edu>
2013-07-31 10:14:58 -0700

    Added data security rules

--- meteor/shared/config.js
+++ meteor/shared/config.js
@@ -6,3 +6,49 @@ Accounts.config({
 	sendVerificationEmail: true,
 	forbidClientAccountCreation: false
 });
+
+Images.allow({
+        insert: function (userId, doc) {
+                if (Meteor.call('isAdmin'))
+                        return true;
+        },
+        update: function (userId, doc, fieldNames, modifier) {
+                if (Meteor.call('isAdmin'))
+                        return true;
+
+        },
+        remove: function (userId, doc) {
+                if (Meteor.call('isAdmin'))
+                        return true;
+        }
+});
+
+Folders.allow({
+        insert: function (userId, doc) {
+                if (Meteor.call('isAdmin'))
+                        return true;
+        },
+        update: function (userId, doc, fieldNames, modifier) {
+                if (Meteor.call('isAdmin'))
+                        return true;
+        },
+        remove: function (userId, doc) {
+                if (Meteor.call('isAdmin'))
+                        return true;
+        }
+});
+
+Annotations.allow({
+        insert: function (userId, doc) {
+                if (Meteor.call('isAdmin'))
+                        return true;
+        },
+        update: function (userId, doc, fieldNames, modifier) {
+                if (Meteor.call('isAdmin'))
+                        return true;
+        },
+        remove: function (userId, doc) {
+                if (Meteor.call('isAdmin'))
+                        return true;
+        }
+});
