860d2f110451d1d6fd62ad9d2a8902584d5df19d
Petr Baudis <pasky@ucw.cz>
2013-09-12 02:02:54 +0200

    lf-render render(): Use linear min/mag filter inst. of nearest, much smoother images

--- meteor/client/compatibility/lf-render.js
+++ meteor/client/compatibility/lf-render.js
@@ -335,8 +335,8 @@ LightFieldRenderer.prototype.render = function(is_new_image) {
 		gl.bindTexture(gl.TEXTURE_2D, this.texture[mode]);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
 	} else {
